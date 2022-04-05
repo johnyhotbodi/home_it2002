f775f9fcd301eb368596d203abe791e961336516
johnyhotbodi <71010589+johnyhotbodi@users.noreply.github.com>
2022-04-05 17:18:53 +0800

    Update admin_view.py

--- app/admin_view.py
+++ app/admin_view.py
@@ -1,7 +1,8 @@
+from multiprocessing.sharedctypes import Value
 import pandas as pd
 import datetime as dt
 from bokeh.io import cardoc
-from bokeh.plotting import figure
+from bokeh.plotting import figure, show
 from bokeh.models import CategoricalColorMapper
 from bokeh.palettes import Spectrals
 from bokeh.layouts import widgetbox, row, gridplot
@@ -12,31 +13,31 @@ data = pd.read_csv('myapp/data/it2002.csv')
 
 ## change the type formatting here
 data['country'] = data['country_str'].astype(str)
-data
+data['date'] = pd.to_datetime(data['date']).dt.date
 
 ##Resetting index and groupby here
-consumer = data
-properties = data
-active = data
-grouped = data
+consumer = data[['country code', 'index']].groupby(['country code']).sum().reset_index()
+properties = data[['country', 'index']].groupby(['country']).sum().reset_index()
+active = data[[]].groupby([]).sum().reset_index()
+grouped = data[['contry', 'index']].groupby(['country code']).sum().sort_values(by= '', ascending= False)
 
-## indexing
-ints_consumer = data[].groupby([]).sum().reset_index()
-ints_properties = 
-ints_active = 
+## list indexing
+ints_consumer = consumer[].value.counts().sort_index().index.tolist()
+ints_properties = properties[].value.counts().sort_index().index.tolist()
+ints_active = active[].value.counts().sort_index().index.tolist()
 
 
 source_consumer = ColumnDataSource(data=[
-    'Total consumers'   :
-    'since'             :
-    'Total_Revenue'     :
-    'country'           :
+    'Total consumers'   : consumer[consumer[]] == 304]['consumer_code'],
+    'since'             :,
+    'Total_Revenue'     :,
+    'country'           :,
 ])
 
 source_properties = ColumnDataSource(data=[
-    'Country'   :
-    'Amenities'             :
-    ''     :
+    'Country'    : properties[consumer[]] == 1576]['properties_code']
+    'Amenities'  :
+    ''              :
     ''
 ])
 
@@ -50,6 +51,7 @@ source_active = ColumnDataSource (data=[
 source_top_country = ColumnDataSource(grouped)
 country = source_top_country.data['country'].tolist()
 
+#Hover tooltips
 tooltips_consumer = [
     ('Cinema Code', '@cinema_code'),
     ('Date', '@data{%F}'),
@@ -71,6 +73,8 @@ tooltips_consumer_vbar = [
     ('Total Revenue', '@total revenue'),
 ]
 
+
+#Plotting the graph
 fig_consumer = figure(x_axis_type = 'datetime',
     plot_height = 500, plot_width=1000,
     title = 'Total Sales across time',
@@ -107,4 +111,73 @@ fig_properties.line('date', 'total_sales',
 
 fig_consumers_vbar.vbar(x='contry_str', top =' total_sales', source= source_top_country, width=0.50)
 
-idx
+### Skipped the gridpoint part
+
+def update_consumers(attr, old, new):
+
+    [start,end] = slider.Value
+    date_from = dt.datetime.fromtimestamp(start).date()
+    date_until = dt.datetime.fromtimestamp(end).date()
+    consumer_cd = int(consumer_select.value)
+
+    #new data
+    cinema_date = cinemas[(cinemas['date'] >= date_since
+
+    new_data = {
+        'the four columns'
+    }
+    source.data = new_data
+
+    fig_consumer.title.text = 'Total Sales Cinema' + consumer_select.value
+
+init_value = (data['date_since'].min(), data['date_since'].max())
+slider = DataRangeSlider(start = init_value[0], end = init_value[1], value = init_value)
+slider.on_change('value', update_consumers)
+
+consumer_select = Select(
+    options = [ str(x) for x in ints]
+    value = '304'
+    title =  'Consumer by Country'
+)
+consumer_select.on_change('value', update_consumer)
+
+def update_properties(attr, old, new):
+
+    [start,end] = slider.Value
+    date_from = dt.datetime.fromtimestamp(start).date()
+    date_until = dt.datetime.fromtimestamp(end).date()
+    properties_cd = int(properties_select.value)
+
+    #new data
+    properties_date = properties[(properties['date'] >= date_from  & (properties['date'] <= date_until)]
+
+    new_data = {
+        'the four columns'
+    }
+    source.data = new_data
+
+    fig_consumer.title.text = 'Total Sales Cinema' + consumer_select.value
+
+init_value = (data['date_since'].min(), data['date_since'].max())
+slider = DataRangeSlider(start = init_value[0], end = init_value[1], value = init_value)
+slider.on_change('value', update_properties)
+
+properties_select = Select(
+    options = [str(x) for x in ints_properties]
+    value = '1576'
+    title =  'properties count'
+)
+properties_select.on_change('value', update_properties)
+
+# Create layout
+layout_0 = 
+layout = row(widgetbox(consumer_select, slider), fig_consumer)
+layout_properties = row(widgetbox(properties_select, slider), fig_properties)
+
+zeroth_panel = Panel (child= layout_0, title= 'Home')
+first_panel = Panel (child= layout, title= 'Top Sales by country')
+second_panel = Panel (child= layout_properties, title= 'Properties by country')
+
+tabs = Tabs(tabs=[first_panel, second_panel])
+
+curdoc().add_root(tabs)
