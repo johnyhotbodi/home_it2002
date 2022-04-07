fdb17145ed0f9fc7abbe93626bed3c576d995dd1
johnyhotbodi <71010589+johnyhotbodi@users.noreply.github.com>
2022-04-07 16:12:53 +0800

    Update geemap.py

--- app/geemap.py
+++ app/geemap.py
@@ -25,13 +25,9 @@ gdf = gpd.read_postgis(sql, con=engine)
 # In[4]:
 
 
-import geemap
-m = geemap.Map(center=[1.351, 103.8198], zoom=6, ee_initialize=False) #center at singapore
-m
-
-
-# In[5]:
-
+import ee
+import geemap.foliumap as geemap
+m = geemap.Map(center=[1.351, 103.8198], zoom=6,) #center at singapore
 
 #area
 style = {
@@ -47,23 +43,28 @@ gdf_crs = gdf.to_crs(epsg="4326")
 geojson = gdf_crs.__geo_interface__
 m.add_geojson(geojson, style=style, layer_name="land")
 
-
-# In[6]:
-
-
 #points
 sql2 = 'SELECT * FROM geometry_test2'
 gdf_prop = gpd.read_postgis(sql2, con=engine)
-gdf_prop = gdf_prop.set_index("name")
+gdf_prop = gdf_prop.set_index("userid")
 gdf_prop_crs = gdf_prop.to_crs(epsg="4326")
 prop_geojson = gdf_prop_crs.__geo_interface__
 m.add_geojson(prop_geojson, layer_name="properties")
 
 
-# out_dir = os.path.expanduser('~/Downloads')
-# if not os.path.exists(out_dir):
-#     os.makedirs(out_dir)
-# out_json = os.path.join(out_dir, 'ne_50m_land.geojson')
-# gdf.to_file(out_json, driver="GeoJSON")
-# out_shp = os.path.join(out_dir, 'ne_50m_land.shp')
-# gdf.to_file(out_shp)
+# In[4]:
+
+
+m
+
+
+# In[6]:
+
+
+m.publish(
+    name='Folium map',
+    description='A folium map with Earth Engine data layers',
+    visibility = 'Public',
+    overwrite = True
+)
+
